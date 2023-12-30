2abd895cf8c3c00d7c4673bbf8e2e588b6c2474a
arensagun2 <arensagun2@gmail.com>
2023-12-30 07:01:33 -0500

    moved to nextjs

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import Navigation from "../navigation"
+
+export default function About() {
+    return (
+        <div className="flex items-center min-h-screen flex-col p-0">
+            <Navigation />
+            <h1>About Page</h1>
+        </div>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Navigation from './navigation'
+
+export default function Home() {
+  return (
+    <main className="flex items-center min-h-screen flex-col p-0">
+      <Navigation />
+      <h1>Home Page</h1>
+    </main>
+  )
+}
