9a25bbdde56fef469f28860c2bfadb1e151e4a4a
arensagun2 <arenjosiah.sagun001@umb.edu>
2024-04-10 19:45:01 -0400

    error fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import NavBar from './components/NavBar';
 import ThemeSwitch from './components/ThemeSwitch';
 
-export default function Home({ children }: { children : React.ReactNode }) {
+export default function Home() {
   return (
     <main className='h-screen'>
       <NavBar />
